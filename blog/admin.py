4ba4c93655cfd3b9d0f291f9afce887fe98decc5
Janbert <janbertw@gmail.com>
2015-05-14 13:40:30 +0800

    My Django Girls app, first commit

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Post
+
+# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+	fields = ['title', 'author', 'text', 'published_date']
+
+admin.site.register(Post, PostAdmin)
